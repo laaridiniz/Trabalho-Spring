af19406997f39b563b706ed6b5f5bd28e07cf867
laaridiniz <diniz.laari@gmail.com>
2024-04-28 17:24:01 -0300

    spring setup

---
+++ src/main/java/br/gov/sp/fatec/anttrabalho/AntTrabalhoApplication.java
@@ -0,0 +1,13 @@
+package br.gov.sp.fatec.anttrabalho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AntTrabalhoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AntTrabalhoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/gov/sp/fatec/anttrabalho/AntTrabalhoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gov.sp.fatec.anttrabalho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AntTrabalhoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
